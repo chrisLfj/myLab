f0e00d0a83057d015d56e8bad31458d7ffc3b620
liufangjun <chris_lfj@163.com>
2021-03-11 23:32:14 +0800

    移动零和最大面积

---
+++ corejava/src/main/java/com/myLab/corejava/algorithm/MaxArea.java
@@ -0,0 +1,39 @@
+package com.myLab.corejava.algorithm;
+
+/**
+ * leetcode 11 盛最多水的容器
+ * 这一题其实是求最大面积的
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 说明：你不能倾斜容器。
+ */
+public class MaxArea {
+    /**
+     * 解题思路：
+     * 1.粗暴方法是，循环套循环，即先用第一个元素分别跟其它n-1个元素求面积，然后再用第二个元素跟其它n-2个元素求面积
+     * 最后取这些面积中最大的一个即得到结果，但是这种方法的时间复杂度是O(n^2)
+     * 2.优化方法是，使用一次循环即可达到效果，目的是减少求面积的次数，原理是两个指针i和j分别从数组的头和尾向中间移动，
+     * 如果i的元素大于j的元素，则i不动，只移动j，移动之前先求面积，反之则j不动，只移动i，直到i和j重合，这个过程中最大的面积即结果
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            if (height[i] <= height[j]) {
+                int area = height[i++] * (j - i + 1);
+                maxArea = Math.max(maxArea, area);
+            } else {
+                int area = height[j--] * (j - i + 1);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        MaxArea maxArea = new MaxArea();
+        System.out.println(maxArea.maxArea(height));
+    }
+}
---
+++ corejava/src/main/java/com/myLab/corejava/algorithm/MoveZeros.java
@@ -0,0 +1,37 @@
+package com.myLab.corejava.algorithm;
+
+/**
+ * leetcode 283 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序
+ * 必须在原数组上操作，不能拷贝额外的数组。尽量减少操作次数。
+ */
+public class MoveZeros {
+    /**
+     * 解题思路：
+     * 1.最常想到的一种思路是，遍历数组，只要发现不为0的元素就将其按顺序放到一个新的数组中，然后在后面补0即可
+     * 不过这个方法需要额外申请一块新数组的内存，不太好
+     * 2.一种好的方式是，只操作原数组即可实现移动零，思路就是零和非零元素进行交换，即准备两个指针i和j，它们同时
+     * 从0开始遍历数组，当走到0元素的时候指针j停止前进，i则继续前进，当i走到非零元素是就跟j所指的元素进行交换，
+     * 这样就能够实现0移动到数组后面，并且非零元素还保持原有顺序不变
+     * @param nums
+     */
+    public void moveZeros(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = 0;
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        MoveZeros moveZeros = new MoveZeros();
+        moveZeros.moveZeros(nums);
+        System.out.println(nums);
+    }
+}
