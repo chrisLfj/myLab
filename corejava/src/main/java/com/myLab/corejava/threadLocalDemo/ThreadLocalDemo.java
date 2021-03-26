5a61b80c104dd4b97eb60407d43070eeb65ff08e
chris_lfj <chris_lfj@hotmail.com>
2021-03-26 18:58:43 +0800

    threadlocaldemo

--- corejava/src/main/java/com/myLab/corejava/threadLocalDemo/ThreadLocalDemo.java
+++ corejava/src/main/java/com/myLab/corejava/threadLocalDemo/ThreadLocalDemo.java
@@ -6,7 +6,9 @@ package com.myLab.corejava.threadLocalDemo;
  * 如果存放的是基础类型或者其对应的封装类型是可以实现隔离的
  * 如果存放的是对象则无法实现隔离，因为threadlocal存的是引用类型，对象的值如果在外部被改变了，那通过threadlocal来获取该对象时，会发现它也随之改变了
  * 因此对于threadlocal的使用需要注意，如果要存放对象的话，那是不是应该先将其深度clone，然后将clone的对象存入threadlocal中
- * 正常情况如spring jdbc connection放到threadlocal，保证事务中获取的是同一个连接
+ * 所以threadlocal是无法实现共享变量的线程安全的，它真正的目的是将变量的范围控制在线程内部，然后别的线程访问不到这个变量，
+ * 这就要求该变量需要在线程内部创建出来，然后放在threadlocal中实现该线程中的程序对此变量共享，然后随着线程的消亡而回收
+ * 例如spring jdbc connection放到threadlocal，它就是在线程内部创建连接，保证事务中获取的是同一个连接
  */
 public class ThreadLocalDemo {
     private static Integer shareNum = 0;
