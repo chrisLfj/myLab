158f61897155e5406be359c375f99a9a18572a2a
chris_lfj <chris_lfj@hotmail.com>
2021-06-08 14:27:27 +0800

    leet code

--- corejava/src/main/java/com/myLab/corejava/algorithm/LCEffectiveBracket.java
+++ corejava/src/main/java/com/myLab/corejava/algorithm/LCEffectiveBracket.java
@@ -4,6 +4,10 @@ import java.util.Stack;
 
 /**
  * leetcode:有效的括号  第20题
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
  */
 public class LCEffectiveBracket {
     /**
